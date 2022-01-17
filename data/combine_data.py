b9ff287d30aaef8c92be6bc9ba547139225554c7
Gavin Anderegg <gavinanderegg@users.noreply.github.com>
2022-01-17 09:18:15 -0400

    Combined data files

--- data/combine_data.py
+++ data/combine_data.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
-cases = pd.read_csv('./csv/cases.csv')
-hosp = pd.read_csv('./csv/hospitalizations.csv')
+cases = pd.read_csv('./csv/cases.csv', usecols=[0, 1, 4], parse_dates=[0])
+hosp = pd.read_csv('./csv/hospitalizations.csv', parse_dates=[0])
 
-breakpoint()
+data = pd.merge(cases, hosp)
+
+data.to_excel('data.xlsx', 'Combined COVID Data', index=False)
+data.to_csv('data.csv', index=False)
+
 
