5a98f20add3d3f12ac87f115a1070737f77f06e3
Gavin Anderegg <gavinanderegg@users.noreply.github.com>
2022-01-17 14:28:32 -0400

    Line drawing

--- js/main.js
+++ js/main.js
@@ -4,8 +4,42 @@ var svg = d3.select('svg#mainData');
 
 var parseDate = d3.timeParse('%Y-%m-%d');
 
+var x = d3.scaleTime().range([0, svg.node().clientWidth]);
+var y = d3.scaleLinear().range([svg.node().clientHeight, 0]);
+
+var casesLine = d3.line()
+    .x(function (d) { return x(d.Date); })
+    .y(function (d) { return y(d.Cases); });
+
+var hospsLine = d3.line()
+    .x(function (d) { return x(d.Date); })
+    .y(function (d) { return y(d.Hosps); });
+
+var deathsLine = d3.line()
+    .x(function (d) { return x(d.Date); })
+    .y(function (d) { return y(d.Deaths); });
+
+var chart = svg.append('g')
+    .attr('class', 'lines');
+
 d3.csv('./data/data.csv', accessor).then((data) => {
-    console.log(data);
+    x.domain(d3.extent(data, function (d) { return d.Date; }));
+    y.domain([0, d3.max(data, function (d) { return d.Cases; })]);
+
+    chart.append('path')
+        .datum(data)
+        .attr('class', 'line casesLine')
+        .attr('d', casesLine);
+
+    chart.append('path')
+        .datum(data)
+        .attr('class', 'line hospsLine')
+        .attr('d', hospsLine);
+
+    chart.append('path')
+        .datum(data)
+        .attr('class', 'line deathsLine')
+        .attr('d', deathsLine);
 }).catch(function (error) {
     console.log(error);
 });
@@ -15,7 +49,10 @@ function accessor(d) {
     d.Cases = +d['Daily new cases'];
     delete d['Daily new cases'];
     d.Deaths = +d['Deaths'];
-    d.Hosp = +d['Total in hospital (both ICU and non-ICU)'];
+    d.Hosps = +d['Total in hospital (both ICU and non-ICU)'];
     delete d['Total in hospital (both ICU and non-ICU)'];
     return d;
 }
+
+
+
